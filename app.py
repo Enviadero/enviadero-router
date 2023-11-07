1795b8bd36d0d9724fc8f23af7a068385d805ebe
Macias Romero Jorge Humberto <macias_george@outlook.com>
2023-11-07 10:30:55 -0600

    test fixed with doto target

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def router():
                     business_code = item["business_code"]
                     break
 
-    if business_code == "enviadero":
+    if business_code == "doto":
         requests.post("https://hook.us1.make.com/c388wduk5ntwmr5cpbf0u9k87ssrjhpx", json=data)
         print("Data success sent for Doto")
         return "Data success sent for Doto"
