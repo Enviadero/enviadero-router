cd69b0a14b8c55cd7d87fe6d0e1e09dcf4e3cc0b
Macias Romero Jorge Humberto <macias_george@outlook.com>
2023-11-10 19:46:01 -0600

    doto webhooks added

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def router():
                     break
 
     if business_code == "doto":
-        # requests.post("https://hook.us1.make.com/c388wduk5ntwmr5cpbf0u9k87ssrjhpx", json=data)
+        requests.post("https://sandboxshipmentsapi.doto.com.mx/api/v1/webhooks/enviadero/tracker", json=data)
+        requests.post("https://shipmentsapi.doto.com.mx/api/v1/webhooks/enviadero/tracker", json=data)
         print("Data success sent for Doto")
         return "Data success sent for Doto"
     elif business_code == "xiaomi":
