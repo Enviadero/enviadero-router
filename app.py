b61cb6e6283a72c6b8b6a4e3b8663f7408048545
Macias Romero Jorge Humberto <macias_george@outlook.com>
2023-11-15 19:16:02 -0600

    Optimized code

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/')
+
+@app.route("/")
 def main():
     return "Ok"
 
-@app.route('/webhook', methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 def router():
     data = request.json
     print(data)
@@ -18,20 +20,29 @@ def router():
 
     labels = {}
     business_code = None
+    groups = data.get("groups", [])
 
     for item in business.json():
-        labels[item["business_code"]] = requests.get(f"https://tim-loonie-94123.herokuapp.com/business/{item['id']}/labels")
+        if groups and data["groups"][0].get("name", "").upper() == item["name"].upper():
+            business_code = item["business_code"]
+            break
+        else:
+            labels[item["business_code"]] = requests.get(
+                f"https://tim-loonie-94123.herokuapp.com/business/{item['id']}/labels")
 
-        for label in labels[item["business_code"]].json():
-            beetrack_dispatch_response = label.get("beetrack_dispatch_response")
-            if beetrack_dispatch_response and "identifier" in beetrack_dispatch_response.get("response", {}):
-                if beetrack_dispatch_response["response"]["identifier"].lower() == data["identifier"].lower() or ('groups' in data and data['groups'] and 'name' in data['groups'][0] and data['groups'][0]['name'].upper() == item["name"].upper()):
-                    business_code = item["business_code"]
-                    break
+            for label in labels[item["business_code"]].json():
+                beetrack_dispatch_response = label.get(
+                    "beetrack_dispatch_response")
+                if beetrack_dispatch_response and "identifier" in beetrack_dispatch_response.get("response", {}):
+                    if beetrack_dispatch_response["response"]["identifier"].lower() == data["identifier"].lower():
+                        business_code = item["business_code"]
+                        break
 
     if business_code == "doto":
-        requests.post("https://sandboxshipmentsapi.doto.com.mx/api/v1/webhooks/enviadero/tracker", json=data)
-        requests.post("https://shipmentsapi.doto.com.mx/api/v1/webhooks/enviadero/tracker", json=data)
+        requests.post(
+            "https://sandboxshipmentsapi.doto.com.mx/api/v1/webhooks/enviadero/tracker", json=data)
+        requests.post(
+            "https://shipmentsapi.doto.com.mx/api/v1/webhooks/enviadero/tracker", json=data)
         print("Data success sent for Doto")
         return "Data success sent for Doto"
     elif business_code == "xiaomi":
@@ -67,4 +78,4 @@ def router():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='3000', debug=True)
+    app.run(host="0.0.0.0", port="3000", debug=True)
